2115ddfc200ab67cfac55e7b026c665e62474fe2
SRIDHARRSHAN SEENIVASAN <168655447+Sridharsh7@users.noreply.github.com>
2024-06-28 19:11:45 +0530

    Time: 39 ms (26.31%) | Memory: 16.3 MB (92.26%) - LeetSync

--- Leetcode/742-to-lower-case/to-lower-case.py
+++ Leetcode/742-to-lower-case/to-lower-case.py
@@ -1,9 +1,3 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        result=""
-        for i in s:
-            if 'A'<= i <= 'Z':
-                result= result + chr(ord(i)+32)
-            else:
-                result=result + i
-        return result
+        return(s.lower())
